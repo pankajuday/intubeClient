b26e111299cf2abd9b3be018861df6397c1454b3
pankaj <pankajuday7033@gmail.com>
2025-02-17 19:16:15 +0530

    UPDATED Env for proxy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: `${process.env.VITE_API_URL_ROOT}`,
         changeOrigin: true,
       },
     },
   },
   plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
 });
